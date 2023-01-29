14397054195500a5294ac19426df6739998b7b50
Dmitrii <my@dimka89.ru>
2023-01-29 20:34:51 +0300

    ПÑsome cheng

---
+++ src/main/java/org/example/index/script.js
@@ -0,0 +1,7 @@
+(function(){
+    document.addEventListener('DOMContentLoaded', function(){
+        let text = document.getElementById('text').value;
+        alert('Длина текста: ' + text.length);
+    });
+    });
+})();
